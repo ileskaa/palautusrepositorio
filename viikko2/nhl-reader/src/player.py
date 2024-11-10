6eaad7010990c24f478242cc690a7227791918ec
ileskaa <akseli.ingervo@gmail.com>
2024-11-10 17:36:15 +0200

    Complete exercise 10

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.team = dict['team']
+        self.games = dict['games']
+        self.id = dict['id']
+
     def __str__(self):
-        return self.name
+        playerstr = f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+        return playerstr
