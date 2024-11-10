dc4fafdf05d1db903e1cc8a3458e6ebb7a41cf8d
ileskaa <akseli.ingervo@gmail.com>
2024-11-10 15:23:27 +0200

    Revert "Merge branch 'main' into bugikorjaus"
    This reverts commit a626cad061b183340b8393b20c39169b98f8eb25, reversing
    changes made to bb46ca2106ddbb72e742dadd1860b5add3567da9.

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -5,14 +5,11 @@ from summa import summa
 from erotus import erotus
 
 logger("aloitetaan ohjelma")
-logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
-logger("lopetetaan ohjelma")
-print("goodbye!")
 logger("lopetetaan ohjelma")
 print("goodbye!")
