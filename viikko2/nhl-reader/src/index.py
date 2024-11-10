b393d0420361dac21a75decc74a872abf5cd1401
ileskaa <akseli.ingervo@gmail.com>
2024-11-10 21:51:38 +0200

    Complete exercise 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,26 +2,39 @@ import requests
 from player import Player
 
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = self.reader.get_players()
+        filter_obj = filter(lambda player: player.nationality == nationality, players)
+        filtered_players = list(filter_obj)
+        filtered_players.sort(key=lambda player: player.goals + player.assists, reverse=True)
+        return filtered_players
+
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    # print("JSON-muotoinen vastaus:")
-    # print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    players.sort(key=lambda player: player.goals + player.assists, reverse=True)
-
-    print("Players from FIN\n")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
-        if player.nationality == 'FIN':
-            print(player)
+        print(player)
 
 
 if __name__ == "__main__":
